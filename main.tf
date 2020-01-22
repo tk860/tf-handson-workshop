86f6a39ed58109393d9972bf6268350ae7adc968
User <user@Student.local>
2020-01-22 17:01:42 +0900

    added tags

--- main.tf
+++ main.tf
@@ -13,5 +13,8 @@ resource "aws_instance" "hello-tf-instance" {
   ami = var.ami
   count = var.hello_tf_instance_count
   instance_type = var.hello_tf_instance_type
+  tags = {
+    owner = "Kabu"
+    ttl   = "100"
+  }
 }
-
